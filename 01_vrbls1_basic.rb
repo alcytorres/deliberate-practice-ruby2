03201109bd4e7f85e4a8ee81be15e64c03ad76e5
Alcy Torres <mxalcytorres@gmail.com>
2024-05-10 03:28:29 -0400

    my first commit

---
+++ 01_vrbls1_basic.rb
@@ -0,0 +1,42 @@
+#1. Write a program that uses a variable to store a number, then prints that number plus 10.
+number = 7
+puts number + 10
+
+#2. Write a program that uses a variable to store a word, then prints that word with all capital letters.
+word = "hello"
+puts word.upcase
+
+#3. Write a program that uses variables to store two numbers, then prints the numbers added together.
+number1 = 7
+number2 = 23
+puts number1 + number2
+
+# 4. Write a program that uses a variable to store a word, then prints that word in reverse order.
+word = "Hello"
+puts word.reverse
+
+# 5. Write a program that uses a variable to store a number, then prints the number times 10.
+number = 30
+puts number * 10
+
+# 6. Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+word1 = "Hello "
+word2 = "World"
+puts word1.upcase + word2.upcase
+
+# 7. Write a program that uses a variable to store a word, then prints the number of letters in the word.
+word = "hello"
+puts word.length
+
+# 8. Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+number = 7.0
+puts number.to_i
+
+# 9. Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+number1 = 7
+number2 = 10
+puts number1 * number2
+
+# 10. Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+word = "HeLLo"
+puts word.downcase
