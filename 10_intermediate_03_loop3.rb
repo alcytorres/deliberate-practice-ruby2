57c9f86695ceb0d38ebf57dbe2e8529bb364051b
Alcy Torres <mxalcytorres@gmail.com>
2024-07-13 09:04:50 -0500

    Update solutions

--- 10_intermediate_03_loop3.rb
+++ 10_intermediate_03_loop3.rb
@@ -81,6 +81,15 @@ p even_numbers
 # 5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #    For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
+strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+short_strings = []
+strings.each do |string|
+  if string.length < 4
+    short_strings << string
+  end
+end
+p short_strings
+
 
 # 6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #    For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
