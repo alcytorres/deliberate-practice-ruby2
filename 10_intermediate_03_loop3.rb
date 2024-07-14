e5429bd54a1a6fb3050abac3309be467e0131ef8
Alcy Torres <mxalcytorres@gmail.com>
2024-07-14 13:11:55 -0400

    Update solutions

--- 10_intermediate_03_loop3.rb
+++ 10_intermediate_03_loop3.rb
@@ -94,6 +94,17 @@ p short_strings
 # 6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #    For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
+items =  [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+short_names = []
+items.each do |item|
+  if item[:name].length < 6
+    short_names << item
+  end
+end
+p short_names
+
+
+
 
 # 7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #    For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
