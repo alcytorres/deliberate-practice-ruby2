f771c70d261e818c7547e5d9e1db8f30d6bbb2a0
Alcy Torres <mxalcytorres@gmail.com>
2024-08-01 23:57:03 -0400

    Update solutions

--- 13_advanced_02-conds3.rb
+++ 13_advanced_02-conds3.rb
@@ -89,3 +89,24 @@ puts "Discount amount: $ #{order_value * discount}"
 # If the destination is an international shipment:
 # If the weight is less than or equal to 1 kg, the shipping fee is $15.
 # If the weight is greater than 1 kg, the shipping fee is $25.
+
+
+destination = "domestic"
+weight = 4
+
+shipping_fee = 0
+if destination == "domestic"
+  if weight <= 1
+    shipping_fee = 5
+  else
+    shipping_fee = 10
+  end
+elsif destination == "international"
+  if weight <= 1
+    shipping_fee = 15
+  else
+    shipping_fee = 25
+  end
+end
+
+puts "Shipping fee: $#{shipping_fee}"
