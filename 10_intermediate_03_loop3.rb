242be5c6d963b3841800274c382717ce5e52ff2a
Alcy Torres <mxalcytorres@gmail.com>
2024-10-15 23:34:40 -0400

    Update solutions

--- 10_intermediate_03_loop3.rb
+++ 10_intermediate_03_loop3.rb
@@ -35,6 +35,7 @@ p new_numbers
 # 2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #    For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
+
 strings = ["winner", "winner", "chicken", "dinner"]
 w_strings = []
 strings.each do |string|
@@ -57,8 +58,10 @@ p w_strings
 # p w_strings
 
 
+
 # 3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #    For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
 
 items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
 expensive_items = []
@@ -71,8 +74,11 @@ end
 p expensive_items
 
 
+
 # 4. Start with an array of numbers and create a new array with only the even numbers.
 #    For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+
 
 numbers = [2, 4, 5, 1, 8, 9, 7]
 even_numbers = []
@@ -85,8 +91,12 @@ end
 p even_numbers
 
 
+
 # 5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #    For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+
+
 
 strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
 short_strings = []
@@ -102,6 +112,9 @@ p short_strings
 # 6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #    For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
+
+
+
 items =  [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
 short_names = []
 items.each do |item|
@@ -115,6 +128,9 @@ p short_names
 
 # 7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #    For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+
+
+
 
 numbers = [8, 23, 0, 44, 1980, 3]
 low_numbers = []
@@ -130,6 +146,9 @@ p low_numbers
 # 8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #    For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
+
+
+
 strings = ["big", "little", "good", "bad"]
 new_strings = []
 strings.each do |string|
@@ -143,6 +162,9 @@ p new_strings
 # 9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #    For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
+
+
+
 items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
 cheap_items = []
 items.each do |item|
@@ -155,6 +177,9 @@ p cheap_items
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+
+
 
 numbers = [2, 4, 5, 1, 8, 9, 7]
 odd_numbers = []
