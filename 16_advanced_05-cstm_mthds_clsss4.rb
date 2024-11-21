53224c5d53d55e366fdead622c41d0f49a1b1c83
Alcy Torres <mxalcytorres@gmail.com>
2024-11-20 23:47:45 -0500

    Update solutions

--- 16_advanced_05-cstm_mthds_clsss4.rb
+++ 16_advanced_05-cstm_mthds_clsss4.rb
@@ -33,10 +33,13 @@ cart.display_items
 
 
 
+
 # 2. Write a Product class that stores the name, price, and metadata, where metadata is a hash that stores additional information about the product.
 
 
+
 # 3. Write a Playlist class that stores a name and an array of songs with methods to add a song, remove a song, shuffle the songs into a random order, and display all the songs.
+
 
 
 # 4. Write a Contact class that stores the name, age, and contact_info, where contact_info is a hash that stores any additional information about the contact.
