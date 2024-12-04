8235ae70de97450b407c0e4cbeb685baecdb222a
Alcy Torres <mxalcytorres@gmail.com>
2024-12-03 19:50:22 -0500

    Update solutions

--- 15_advanced_04_arrys_hshs3.rb
+++ 15_advanced_04_arrys_hshs3.rb
@@ -376,6 +376,7 @@ items = [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black
 
 
 
+
 prices = {"chair" => 75, "book" => 15}
 items = [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}]
 # {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}
@@ -401,6 +402,7 @@ p combined_hash
 books = [{ author: "Jeff Smith", title: "Bone" }, { author: "George Orwell", title: "1984" }, { author: "Jeff Smith", title: "RASL" }]
 #       {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}
 # books_hash =
+
 
 
 
@@ -464,6 +466,10 @@ p books_hash
 original_hash = { "a" => 1, "b" => 2, "c" => 3 }
 #               {1 => "a", 2 => "b", 3 => "c"}
 # flipped_hash =
+
+
+
+
 
 
 
