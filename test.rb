171f098f8148643c5e9458a625bf75789c928597
Alcy Torres <mxalcytorres@gmail.com>
2024-12-18 23:14:25 -0500

    Update solutions

--- test.rb
+++ test.rb
@@ -128,3 +128,24 @@ end
 p test
 # output: ["chair", 75, "book", 15]
 
+
+
+
+# Imagine we have a method that calculates the sum of two numbers.
+def add_numbers(a, b)
+  # Simple log statement before the calculation
+  puts "DEBUG: add_numbers called with a=#{a}, b=#{b}"
+
+  result = a + b
+
+  # Another log statement after the calculation
+  puts "DEBUG: result of add_numbers is #{result}"
+
+  result
+end
+
+# Somewhere in our code, something unexpected happens:
+sum = add_numbers(10, 5)
+# Output on the console would show:
+# DEBUG: add_numbers called with a=10, b=5
+# DEBUG: result of add_numbers is 15
