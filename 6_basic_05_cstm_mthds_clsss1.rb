e5951da02d1a2e054761018a2cc1afc48f0a3202
Alcy Torres <mxalcytorres@gmail.com>
2025-01-22 22:58:37 -0500

    Update solutions

--- 6_basic_05_cstm_mthds_clsss1.rb
+++ 6_basic_05_cstm_mthds_clsss1.rb
@@ -68,7 +68,11 @@ p convert_to_string(num)
 # 8. Write a method that takes in a string and returns the string repeated 5 times. Then run the method and print the result.
 
 
+
 # 9. Write a method that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the method and print the result.
 
 
+
 # 10. Write a method that takes in a number and returns the number times 10 plus 30. Then run the method and print the result.
+
+
