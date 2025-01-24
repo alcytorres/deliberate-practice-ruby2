bd8f902112e702b8e3b3ae116dc22e5716c233a7
Alcy Torres <mxalcytorres@gmail.com>
2025-01-24 00:24:03 -0500

    Update solutions

--- 3_basic_03_loop1.rb
+++ 3_basic_03_loop1.rb
@@ -1,13 +1,11 @@
 # Write a loop which modifies a variable each time the loop runs
 
-
 # 1. Write a while loop to print the numbers 1 through 10.
-
-# index = 1
-# while index <= 10
-#   puts index
-#   index += 1
-# end
+number = 1
+while number <= 10
+  puts number
+  number += 1
+end
 
 # number = 1
 # while number <= 10
@@ -16,21 +14,21 @@
 # end
 
 # # 2. Write a while loop that prints the word "hello" 5 times.
-# index = 0
-# while index < 5
-#   puts "hello"
-#   index += 1
-# end
+index = 0
+while index < 5
+  puts "hello"
+  index += 1
+end
 
 # # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 # #    REVIEW
-while true
-  puts "Enter a word:"
-  user_input = gets.chomp
-  if user_input == "stop"
-    break
-  end
-end
+# while true
+#   puts "Enter a word:"
+#   user_input = gets.chomp
+#   if user_input == "stop"
+#     break
+#   end
+# end
 
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
@@ -50,19 +48,42 @@ end
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
+# while true
+#   puts "Enter a number: "
+#   input = gets.chomp
+#   if input.to_i > 10
+#     break
+#   end
+# end
 
 # 7. Write a while loop that prints the numbers 50 to 70.
-
+number = 50
+while number <= 70
+  p number
+  number += 1
+end
 
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+# index = 1
+# while index <= 144
+#   puts "Around the world"
+#   index += 1
+# end
 
 
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
-
+# while true
+#   puts "Enter a word"
+#   input = gets.chomp
+#   if input.length > 5
+#     break
+#   end
+# end
 
 # 10. Write a while loop that prints the even numbers from 2 to 40.
-number = 2
-while number <= 40
-  puts i
-  number += 2
-end
+# number = 2
+# while number <= 40
+#   puts number
+#   number += 2
+# end
+
