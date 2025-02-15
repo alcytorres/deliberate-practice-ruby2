0f29a0505c913151797dc27c9226b072a13e3091
Alcy Torres <mxalcytorres@gmail.com>
2025-02-14 19:14:26 -0500

    Update solutions

--- 1_basic_01_vrbls1.rb
+++ 1_basic_01_vrbls1.rb
@@ -1,41 +1,58 @@
 # Write number and string variables with built-in methods
 
-
 #1. Write a program that uses a variable to store a number, then prints that number plus 10.
+
 number = 7
-p number + 10
+puts number + 10
 
 #2. Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
 word = "sky"
 puts word.upcase
 
 #3. Write a program that uses variables to store two numbers, then prints the numbers added together.
+
 number1 = 4
 number2 = 10
-p number1 + number2
+puts number1 + number2
 
 # 4. Write a program that uses a variable to store a word, then prints that word in reverse order.
+
 word = "sky"
-p word.reverse
+puts word.reverse
 
 # 5. Write a program that uses a variable to store a number, then prints the number times 10.
+
 number = 7
 p number * 10
 
 # 6. Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
 word1 = "sky"
 word2 = "ocean"
-puts "#{word1} and #{word2}"
+puts word1.upcase + " " + word2.upcase
+
+# puts "#{word1.upcase} #{word2.upcase}"
 
 # 7. Write a program that uses a variable to store a word, then prints the number of letters in the word.
 
+word = "breaking"
+puts word.length
 
 # 8. Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
 
+number = 3.141
+puts number.to_i
+
+# puts number.to_f
 
 # 9. Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
 
+num1 = 3
+num2 = 5
+puts num1 * num2
 
 # 10. Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
 word = "SKY"
 p word.downcase
