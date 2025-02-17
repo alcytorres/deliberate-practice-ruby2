c22a515918d6c777eeca0fc9e620f44d4279f770
Alcy Torres <mxalcytorres@gmail.com>
2025-02-16 22:35:56 -0500

    Update solutions

--- 5_basic_04_arrys_hshs2.rb
+++ 5_basic_04_arrys_hshs2.rb
@@ -1,21 +1,24 @@
 # Write a hash, read items from a hash, and add new items to existing hash
 
+
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
 
+person = {first_name: "John", last_name: "Smith", email: "johnsmith@gmail.com" }
 
-person = {first_name: "John", last_name: "Smith", email_address: "johnsmith@gmail.com" }
+
 # puts person[:first_name]
 # puts person[:last_name]
-# puts person[:email_address]
+# puts person[:email]
+
 
 # Solution
-# person = {"first_name" => "John", "last_name" => "Smith", "email_address" => "johnsmith@gmail.com" }
+person = {"first_name" => "John", "last_name" => "Smith", "email" => "johnsmith@gmail.com" }
 # puts person["first_name"]
 # puts person["last_name"]
-# puts person["email_address"]
+# puts person["email"]
+
 
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
-
 
 persons = [
   {first_name: "Mike", last_name: "Santana"},
@@ -23,37 +26,38 @@ persons = [
   {first_name: "John", last_name: "Smith"}
 ]
 
-# puts persons[0][:first_name]
-# puts persons[0][:last_name]
 
 
 # Solution
 # puts persons[0][:first_name]
 # puts persons[0][:last_name]
 
-# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
+# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
 menu = { "sandwich" => 4, "soda" => 2, "juice" => 3 }
 
-# puts menu
+
 
 # Solution
 # menu["fries"] = 1
 # puts menu
 
-# # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
 book = {title: "The Adventures of Tom Sawyer", author: "Mark Twain", pages: 200, language: "English"}
 
+
+
+# Solution
 # puts book[:title]
 # puts book[:author]
 # puts book[:pages]
 # puts book[:language]
 
-# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
 books = [
     {title: "To Kill a Mockingbird", author: "Harper Lee"},
@@ -61,29 +65,30 @@ books = [
     {title: "The Great Gatsby", author: "Scott Fitzgerald"}
 ]
 
-# puts books[2][:author]
 
 
 # Solution
 # puts books[2][:author]
+
 
 # 6. Make a hash to store 3 different states and their capitals. Then add a new state and capital and print the hash to see the result.
 
-
 capitals = {"California" => "Sacramento", "Texas" => "Austin", "Florida" => "Tallahassee"}
 
-# puts capitals
 
 
 # Solution
-# capitals["Massachusetts"] = "Boston"
+# capitals[:Massachusetts] = "Boston"
 # puts capitals
+
 
 # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
 
-
 laptop = {brand: "apple", model: "macbook pro", year: 2023 }
 
+
+
+# Solution
 # puts laptop[:brand]
 # puts laptop[:model]
 # puts laptop[:year]
@@ -91,22 +96,22 @@ laptop = {brand: "apple", model: "macbook pro", year: 2023 }
 
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
 
-
 laptop = [
   {brand: "apple", model: "macbook pro"},
   {brand: "dell", model: "xps13"},
   {brand: "windows", model: "lenovo"}
 ]
 
-# puts
+
 
 # Solution
 # puts laptop[1][:model]
 
+
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
-
 dictionary = { good: "of high quality", mad: "very angry" }
+
 
 # puts dictionary
 
@@ -114,10 +119,12 @@ dictionary = { good: "of high quality", mad: "very angry" }
 # dictionary[:glad] = "delighted"
 # puts dictionary
 
+
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
 
-
 shirt = {brand: "nike", color: "white", size: "medium"}
+
+
 
 # Solution
 # puts shirt[:brand]
--- 6_basic_05_cstm_mthds_clsss1.rb
+++ 6_basic_05_cstm_mthds_clsss1.rb
@@ -1,89 +1,134 @@
 # Write a custom method with valid syntax
 
 # 1. Write a method that takes in a number and returns the number times two. Then run the method and print the result.
-def get_double(number)
-  number * 2
-end
 
-num = get_double(10)
-p num
+
+# def get_double(num)
+#   num * 2
+# end
+
+# number = get_double(10)
+# puts number
+
+# puts get_double(10)
+
 
 # 2. Write a method that takes in a string and returns the string with all capital letters. Then run the method and print the result.
-def make_upcase(string)
-  string.upcase
-end
 
-word = "rocket"
-p make_upcase(word)
+
+# def make_upcase(string)
+#   string.upcase
+# end
+
+# word = make_upcase("fly")
+# puts word
+
+
 
 # 3. Write a method that takes in two numbers and returns the first number subtracted by the second. Then run the method and print the result.
-def get_difference(num1, num2)
-  num1 - num2
-end
 
-test = get_difference(10, 3)
-p test
+
+# def get_difference(num1, num2)
+#   num1 - num2
+# end
+
+# numbers = get_difference(10, 5)
+# puts numbers
+
 
 # 4. Write a method that takes in a number and returns the number times itself. Then run the method and print the result.
-def square(number)
-  number * number
-end
 
-num = square(10)
-p num
+
+# def square(num)
+#   num * num
+# end
+
+# number = square(10)
+# puts number
+
 
 # 5. Write a method that takes in a string and returns the first letter of the string. Then run the method and print the result.
-def get_first_letter(string)
-  string[0]
-end
 
-word = "sky"
-p get_first_letter(word)
+
+# def get_first_letter(string)
+#   string[0]
+# end
+
+# word = get_first_letter("fly")
+# puts word
+
 
 # 6. Write a method that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the method and print the result.
 
-def string_combiner(s1, s2, s3)
-  "#{s1} #{s2} #{s3}"
-end
 
-p string_combiner("sky", "fly", "run")
+# def string_combiner(s1, s2, s3)
+#   "#{s1} #{s2} #{s3}"
+# end
+
+# input = string_combiner("a", "b", "c")
+# puts input
 
 
+# def string_combiner(s1, s2, s3)
+#   "#{s1} #{s2} #{s3}"
+# end
 
-def string_combiner(s1, s2, s3)
-  "#{s1} #{s2} #{s3}"
-end
+# input = ["a", "b", "c"]
+# puts string_combiner(*input)
+# Uses the splat operator (*) to "unpack" the 'input' array so that
+# each element is passed as an individual argument to 'string_combiner'.
+# This effectively calls string_combiner("a", "b", "c").
+# The result ("a b c") is then printed to the console.
 
-words = ["sky", "fly", "run"]
-p string_combiner(*words) # Use the splat operator (*) to unpack the array
+
+# def string_combiner(s1, s2, s3)
+#   s1 + " " + s2 + " " + s3
+# end
+
+# input = string_combiner("a", "b", "c")
+# puts input
+
 
 # 7. Write a method that takes in a number and returns the number as a string. Then run the method and print the result.
-def convert_to_string(number)
-  number.to_s
-end
 
-num = 10
-p convert_to_string(num)
+
+# def convert_to_string(num)
+#   num.to_s
+# end
+
+# number = convert_to_string(10)
+# puts number
+
 
 # 8. Write a method that takes in a string and returns the string repeated 5 times. Then run the method and print the result.
-def repeat_strings(strings)
-  strings * 5
-end
 
-p repeat_strings("sky")
+
+# def repeat_strings(strings)
+#   "#{strings} " * 5
+# end
+
+# result = repeat_strings("sky")
+# puts result
+
 
 # 9. Write a method that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the method and print the result.
-def average(num1, num2, num3)
-  return (num1 + num2 + num3) / 3
-end
 
-p average(5, 10, 15)
+
+# def average(num1, num2, num3)
+#   (num1 + num2 + num3) / 3.0
+# end
+
+# numbers = average(5, 10, 15)
+# puts numbers
+
 
 # 10. Write a method that takes in a number and returns the number times 10 plus 30. Then run the method and print the result.
-def convert_number(number)
-  number * 10 + 30
-end
 
-p convert_number(3)
 
+# def convert_number(num)
+#   num * 10 + 30
+# end
+
+# number = convert_number(10)
+# puts number
 
