4d78b70973f3610aa5933354a94777ad5109b4df
Alcy Torres <mxalcytorres@gmail.com>
2025-02-18 17:13:04 -0500

    Update solutions

--- 8_intermediate_01_vrbls2.rb
+++ 8_intermediate_01_vrbls2.rb
@@ -3,39 +3,52 @@
 
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-first_name = "John"
-last_name = "Smith"
+# first_name = "John"
+# last_name = "Smith"
 
-puts first_name + " " + last_name
+# puts first_name + " " + last_name
+
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
-first_name = "Mark"
-last_name = "Smith"
-puts "#{first_name} #{last_name}"
+# first_name = "Mark"
+# last_name = "Smith"
+# puts "#{first_name} #{last_name}"
+
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
-# puts "What is your name?"
-# user_input = gets.chomp
-# if user_input == "marco"
+# puts "Enter a word"
+# input = gets.chomp
+# if input == "marco"
 #   puts "polo"
 # end
 
+# # Advanced Solution --> Continues the loop until the user enters "marco"
+# while true
+#   puts "Enter a word: "
+#   input = gets.chomp
+#   if input == "marco"
+#     puts "polo"
+#     break
+#   end
+# end
+
+
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
-color1 = "blue"
-color2 = "white"
-color3 = "black"
-puts "The three colors are" + " " + color1 + ", " + color2 + ", " + "and " + color3 + "."
+# color1 = "blue"
+# color2 = "white"
+# color3 = "black"
+# puts "The three colors are" + " " + color1 + ", " + color2 + ", " + "and " + color3 + "."
 
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
-color1 = "blue"
-color2 = "white"
-color3 = "black"
-puts "The three colors are #{color1}, #{color2}, and #{color3}."
+# color1 = "blue"
+# color2 = "white"
+# color3 = "red"
+# puts "The three colors are #{color1}, #{color2}, and #{color3}."
 
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
@@ -44,6 +57,14 @@ puts "The three colors are #{color1}, #{color2}, and #{color3}."
 # user_input = gets.chomp
 # if user_input != "Santa"
 #   puts "You're not Santa."
+# end
+
+# puts "Enter a name"
+# input = gets.chomp
+# if input != "Santa"
+#   puts "You're not Santa"
+# else
+#   puts "You are Santa"
 # end
 
 
@@ -56,13 +77,13 @@ puts "The three colors are #{color1}, #{color2}, and #{color3}."
 
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
-puts "Enter a password"
-user_input = gets.chomp
-if user_input == "Joshua"
-  puts "Shall we play a game?"
-else
-  puts "Access denied"
-end
+# puts "Enter a password"
+# user_input = gets.chomp
+# if user_input == "Joshua"
+#   puts "Shall we play a game?"
+# else
+#   puts "Access denied"
+# end
 
 
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
