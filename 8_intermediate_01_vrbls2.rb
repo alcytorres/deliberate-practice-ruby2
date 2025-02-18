9058fd5a40ad6925fc3c5d5cceb9acf8ec2e2767
Alcy Torres <mxalcytorres@gmail.com>
2025-02-17 21:33:04 -0500

    Update solutions

--- 8_intermediate_01_vrbls2.rb
+++ 8_intermediate_01_vrbls2.rb
@@ -1,19 +1,21 @@
 # Insert a variable into a string using either concatenation or interpolation
 
 
-
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
 first_name = "John"
 last_name = "Smith"
 
 puts first_name + " " + last_name
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
 first_name = "Mark"
 last_name = "Smith"
 puts "#{first_name} #{last_name}"
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
 # puts "What is your name?"
 # user_input = gets.chomp
 # if user_input == "marco"
@@ -21,6 +23,7 @@ puts "#{first_name} #{last_name}"
 # end
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
 color1 = "blue"
 color2 = "white"
 color3 = "black"
@@ -28,6 +31,7 @@ puts "The three colors are" + " " + color1 + ", " + color2 + ", " + "and " + col
 
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
 color1 = "blue"
 color2 = "white"
 color3 = "black"
@@ -35,11 +39,13 @@ puts "The three colors are #{color1}, #{color2}, and #{color3}."
 
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
 # puts "What is your name?"
 # user_input = gets.chomp
 # if user_input != "Santa"
 #   puts "You're not Santa."
 # end
+
 
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
@@ -49,6 +55,7 @@ puts "The three colors are #{color1}, #{color2}, and #{color3}."
 
 
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
 puts "Enter a password"
 user_input = gets.chomp
 if user_input == "Joshua"
@@ -58,6 +65,7 @@ else
 end
 
 
-
-
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+
+
